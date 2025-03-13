eb1aba3bd18e3a95720b75dcae5ee5439222f144
EnderJunk <christian.walters1996@gmail.com>
2025-03-12 21:14:27 -0600

    implemented e2e testing

--- cypress/e2e/questionAPI.cy.ts
+++ cypress/e2e/questionAPI.cy.ts
@@ -1,14 +1,17 @@
-// describe('template spec', () => {
-//   it('passes', () => {
-//     cy.visit('https://example.cypress.io')
-//   })
-// })
-
-describe("template spec", () => {
-  it("passes", () => {
-    cy.request({
-      url: "http://127.0.0.1:3001/api/questions/random",
-      method: "GET",
-    });
+describe("tests questions connection, and button existence", () => {
+  it("should find a button with class 'btn-test'", () => {
+    // Visit the page where the button should be located
+    cy.visit("http://127.0.0.1:3001");
+    cy.get(".btn-test").should("exist");
+    cy.get(".btn-test").should("be.visible");
+  });
+});
+it("GET req returns w/ a status code of 200 and has 10 questions", () => {
+  cy.request({
+    url: "http://127.0.0.1:3001/api/questions/random",
+    method: "GET",
+  }).then((resp) => {
+    expect(resp.status).to.equal(200);
+    expect(resp.body).to.have.length(10);
   });
 });
